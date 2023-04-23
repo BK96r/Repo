6e76bc9496d21cb59207bb2fd8b6320626f2ab95
rin216a <rinabd216@gmail.com>
2023-04-23 13:48:52 -0700

    new try

--- src/main/java/Sara_mentoring_group/RinA.java
+++ src/main/java/Sara_mentoring_group/RinA.java
@@ -1,4 +1,9 @@
 package Sara_mentoring_group;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
 public class RinA {
+    public static void main(String[] args) {
+        System.out.println("gitPractice");
+    }
 }
